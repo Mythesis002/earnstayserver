22e8612c12674de60d15e74b61a57cebf47adcce
Mythesis002 <131542998+Mythesis002@users.noreply.github.com>
2024-06-20 14:06:26 +0530

    Update install-chromium.js

--- install-chromium.js
+++ install-chromium.js
@@ -2,9 +2,9 @@ const chromium = require('chrome-aws-lambda');
 
 (async () => {
   try {
-    await chromium.executablePath;
-    console.log('Chromium installed successfully');
+    await chromium.font('https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf');
+    console.log('Chromium setup completed');
   } catch (error) {
-    console.error('Failed to install Chromium:', error);
+    console.error('Failed to set up Chromium:', error);
   }
 })();
